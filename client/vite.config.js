b508705188302ae8e9c5e63f92120b2b8f618066
Apfirebolt <aspper20@gmail.com>
2023-03-22 16:54:08 +0530

    feat: added vite vue frontend render

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    proxy: {
-      '/api': {
-        target: `http://localhost/:8000`,
-        changeOrigin: true,
-      },
-    },
     port: 8080
   },
   build: {
