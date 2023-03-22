b44f8758581c2d2acafd8b51081cc7b717e4c9ef
Apfirebolt <aspper20@gmail.com>
2023-03-22 16:27:16 +0530

    feat: added frontend

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost/:8000`,
+        changeOrigin: true,
+      },
+    },
+    port: 8080
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
